a9a869052fb7158a02cc37fe9330151db11daa10
Andrea <andreademarco91@gmail.com>
2020-01-19 18:54:40 +0100

    Dichiarato anche circle

--- main.js
+++ main.js
@@ -18,6 +18,8 @@ $(document).ready(
 
 var imgActive = $('img.active') //dichiaro classe immagine imgActive
 var imgNext = imgActive.next() //dichiaro immagine successiva a active
+var circActive = $('i.active') //dichiaro classe cerchio imgActive
+var circNext = circActive.next() //dichiaro cerchio successivo ad active
 // COMPORTAMENTO CLICK next
 function clickNext(){
 
